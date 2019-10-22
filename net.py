f410a90078154ba1390f6975af0ccefee06bd59a
Gwent <gwentkrause@gmail.com>
2019-10-22 15:35:12 +0200

    creates the base of the net

--- net.py
+++ net.py
@@ -3,3 +3,21 @@ import torchvision
 from torch import nn
 
 # TODO everything lol
+class EnergyNet(torch.nn.Module):
+	"""
+	Model of a Recurrent Neural Network approach to predict the energy consumption of a building.
+	"""
+	def __init__(self):
+
+		# uses parameters, methods of parent Class
+		super(EnergyNet, self).__init__()
+
+		# TODO add layers
+		...
+
+	def forward(self, x):
+		"""
+		defines how the data is passed from each layer to another.
+		"""
+
+		return x
