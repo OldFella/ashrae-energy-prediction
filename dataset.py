01dfa7039213f498552b7f0e10874f1e544a830c
Gwent <gwentkrause@gmail.com>
2019-10-25 17:41:08 +0200

    new functions for dataset

--- dataset.py
+++ dataset.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
-
+import math
 from datetime import datetime
 
 train = 'train.csv'
@@ -32,11 +32,10 @@ class AshraeDataset(Dataset):
 
 			the input variables:
 			data = [	Building_id, meter, primary_usage, square_feet, year_built, floor_count, site_id, air_temperature,
-					 	cloud_coverage,dew_temperature, precip_depth_1_hr, sea_level_pressure, wind_direction, wind_speed]
+					 	cloud_coverage,dew_temperature, precip_depth_1_hr, sea_level_pressure, wind_direction, wind_speed, month, day, hour]
 			
 			target = meter_reading
 		"""
-
 		data = self.energy_frame.iloc[idx]
 		target = data['meter_reading']
 		building_id = data['building_id']
@@ -60,23 +59,66 @@ class AshraeDataset(Dataset):
 		#get the row with the current site_id and timestamp from wheater data  
 		wheater_data = self.wheater.loc[(self.wheater['site_id'] == site_id) & (self.wheater['timestamp'] == date)]
 		wheater_data = wheater_data.values
-		wheater_data = wheater_data[0]
+		if len(wheater_data) == 0:
+			wheater_data = [-1,-1,-1,-1,-1,-1,-1,-1,-1]
+		else: 
+			wheater_data = wheater_data[0]
 
 		# transform the time form str to int and add individualy day, month and hour
-		time = wheater_data[1]
+		time = date
 		time = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
 		day = time.day
 		hour = time.hour
 		month = time.month
 		wheater_data = np.delete(wheater_data,1)
-		time = np.array([day,hour,month])
+		time = np.array([month,day,hour])
 		wheater_data = np.concatenate([wheater_data, time])
 		data = np.concatenate([data, wheater_data])
-
-
-		#transform output to tensors
+		# print(data)
 		data = data.astype(float)
+		for i in range(len(data)):
+			# print(type(data[i]), data[i])
+			if math.isnan(data[i]):
+				# print('nan--')
+				data[i] = -1
+		#transform output to tensors
 		data = torch.Tensor(data)
 		target = torch.Tensor(np.array([target]))
+		# target = target.astype(np.int_)
 		return data, target
 
+
+	def __getbuildingitem__(self,idx,b_id):
+		building_id_list = self.energy_frame['building_id'].values
+		idxs = self.energy_frame.index[self.energy_frame['building_id'] == b_id].tolist()
+		return self.__getitem__(idxs[idx])
+
+	def getbuildingdata(self,b_id, random_start = False, length = -1):
+
+		building_id_list = self.energy_frame['building_id'].values
+		idxs = self.energy_frame.index[self.energy_frame['building_id'] == b_id].tolist()
+		sequence_length = len(idxs)
+		print(sequence_length)
+		start = 0
+		if random_start:
+			start = np.random.randint(0,sequence_length)
+		data = []
+		target = []
+		sequence_end = start + length
+		for building in range(len(idxs)):
+			current_building = building + start
+			if current_building == sequence_end or current_building >= len(idxs):
+				break
+			current_data, current_target = self.__getitem__(idxs[current_building])
+			data.append(current_data.numpy())
+			target.append(current_target.numpy())
+			# print(data, target)
+		return torch.Tensor(np.array(data)), torch.Tensor(np.array(target))
+
+
+# d = AshraeDataset(train, buildings, wheater)
+
+# # for x in range(1000):
+# print('-')
+# x = d.getbuildingdata(1,random_start = True, length = 10000000000)
+# print(x, len(x[0]))
--- net.py
+++ net.py
@@ -3,21 +3,30 @@ import torchvision
 from torch import nn
 
 # TODO everything lol
-class EnergyNet(torch.nn.Module):
+class PlaceholderNet(nn.Module):
 	"""
 	Model of a Recurrent Neural Network approach to predict the energy consumption of a building.
 	"""
 	def __init__(self):
 
 		# uses parameters, methods of parent Class
-		super(EnergyNet, self).__init__()
+		super(PlaceholderNet, self).__init__()
 
 		# TODO add layers
-		...
+		self.l1 = nn.Linear(in_features = 17, out_features = 50)
+		self.l2 = nn.Linear(in_features = 50, out_features = 1)
+
+		
 
 	def forward(self, x):
 		"""
 		defines how the data is passed from each layer to another.
 		"""
-
+		# print('')
+		# x = nn.functional.relu(x)
+		# print(x)
+		x = self.l1(x)
+		# print(x)
+		x = self.l2(x)
+		# print(x)
 		return x
